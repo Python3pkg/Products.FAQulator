9116f6781826c2e2b3fae97a2c831b04bf841888
v.pretre <v.pretre@35850d7f-6862-7b4e-9bfd-3656bc855f3a>
2013-01-29 16:27:34 +0000

    Reorganized Products.FAQulator sources
    git-svn-id: https://svn.vmm.be/svn/Webteam/buildouts/kennis.vmm.be/branches/plone_4/src/Products.FAQulator@3734 35850d7f-6862-7b4e-9bfd-3656bc855f3a

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.6'
+
+setup(name='Products.FAQulator',
+      version=version,
+      description="",
+      long_description="",
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
